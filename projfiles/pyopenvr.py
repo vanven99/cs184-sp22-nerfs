123d7f6c68bd15515e5d8ef59a8b4160d01ec8c2
cy294 <lolwtfwhydoihavetofillthisout123@gmail.com>
2022-05-03 20:05:20 -0700

    communication directly with instant-ngp!

--- projfiles/pyopenvr.py
+++ projfiles/pyopenvr.py
@@ -16,29 +16,34 @@ poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
 #Pipe code for interprocess communication: Images NOT supported yet, still figuring out how to recover
 #PNG images from bytes and display them to the vive
 
-#handle = win32file.CreateFile(
-#                r'\\.\pipe\Foo',
-#                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
-#                0,
-#                None,
-#                win32file.OPEN_EXISTING,
-#                0,
-#                None
-#            )
+handle = win32file.CreateFile(
+                r'\\.\pipe\Foo',
+                win32file.GENERIC_READ | win32file.GENERIC_WRITE,
+                0,
+                None,
+                win32file.OPEN_EXISTING,
+                0,
+                None
+            )
 
-#resp = win32file.ReadFile(handle, 64*1024)
-#print(f"message: {resp}")
+resp = win32file.ReadFile(handle, 64*1024*10000)
+print(f"message: {resp}")
 
-overlay = openvr.VROverlay()
-key = "image"
-name = "image"
+png_array = list(resp[1])
+print("png array: ", png_array)
+print("length: ", len(png_array))
+
 #ov = overlay.createOverlay(key, name)
 
 #OpenVR gets the NeRF image by reading from a file. Will replace with the far
 #more efficient pipe method for interprocess communication when able.
 # For now, putting cat.png into both eyes.
-GLUint_L = texture.texture("cat.png", 1800)
-GLUint_R = texture.texture("cat.png", 1800)
+
+GLUint_L = texture.texture_from_bytes(png_array, 100, 100)
+GLUint_R = texture.texture_from_bytes(png_array, 100, 100)
+
+#GLUint_L = texture.texture("cat.png", 1800)
+#GLUint_R = texture.texture("cat.png", 1800)
 
 left_eye_texture = openvr.Texture_t()
 left_eye_texture.eType = openvr.TextureType_OpenGL
@@ -54,13 +59,14 @@ try:
     openvr.VRCompositor().submit(openvr.Eye_Right, right_eye_texture)
 except:
     print("failed")
-# openvr.VRCompositor().submit(openvr.Eye_Right, left_eye_texture)
 
+# openvr.VRCompositor().submit(openvr.Eye_Right, left_eye_texture)
 
 #overlay.setOverlayFromFile(ov, "C:/Users/Cyrus/Desktop/nerfsProj/projfiles/fox_base.png")
 #overlay.showOverlay(ov
 #
 #
+
 poses, _ = openvr.VRCompositor().waitGetPoses(poses, None)
 #print(poses)
 #openvr.VRCompositor().fadeGrid(1, 1)
--- projfiles/texture.py
+++ projfiles/texture.py
@@ -10,6 +10,10 @@ def texture(filename, size):
     png = Image.open(filename)
     bytes = np.asarray(png)
     bytes = np.flipud(bytes)
+    #print(png)
+
+
+    
 
     #opengl context
     config = pyglet.gl.Config()
@@ -28,3 +32,29 @@ def texture(filename, size):
     return ID
 # if __name__ == "__main__":
 #     texture()
+
+
+def texture_from_bytes(bytes, width, height):
+    #convert image to bytes
+    #opengl context
+    config = pyglet.gl.Config()
+
+    #define gl ID
+    pixel_array = []
+    byte_array = []
+    for i in range(width * height):
+        pixel_array.append(np.asarray(bytes[4 * i : 4 * i + 3]))
+    for i in range(height):
+        byte_array.append(np.asarray(pixel_array[width * i : width * i + width]))
+    final_array = np.asarray(byte_array)
+    ID = glGenTextures(1)
+    #bind ID to byte array
+    glBindTexture(GL_TEXTURE_2D, ID)
+    #print(bytes)
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGB,
+        GL_UNSIGNED_BYTE, final_array)
+
+    #tell renderer how to handle minification/magnification cases
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+    return ID
